57b2a08f198c6c11838a5af267c270971234f255
WangWang <lichengwang@kkstream.com>
2021-09-03 00:30:23 +0800

    remove useless code

--- app/src/main/java/com/example/wangsamplecode/MainActivity.kt
+++ app/src/main/java/com/example/wangsamplecode/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.wangsamplecode
 
-import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.LinkProperties
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.wangsamplecode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    companion object {
+        var isNetworkConnected = false
+    }
 
     val TAG = MainActivity::class.java.simpleName
 
@@ -27,32 +30,33 @@ class MainActivity : AppCompatActivity() {
 
         val connectivityManager = getSystemService(ConnectivityManager::class.java)
 
-//        val linkProperties = connectivityManager.getLinkProperties(currentNetwork)
-
-        binding.button.setOnClickListener{
-            val currentNetwork = connectivityManager.getActiveNetwork()
-            val caps = connectivityManager.getNetworkCapabilities(currentNetwork)
-
-            if(caps==null) Log.d(TAG,"no network now")
-            else Log.d(TAG,"network is connected now")
+        binding.button.setOnClickListener {
+            Log.d(TAG, "is network connected:$isNetworkConnected ")
         }
 
-
-
-        connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network : Network) {
+        connectivityManager.registerDefaultNetworkCallback(object :
+            ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
                 Log.d(TAG, "The default network is now: " + network)
+                isNetworkConnected = true
             }
 
-            override fun onLost(network : Network) {
-                Log.d(TAG, "The application no longer has a default network. The last default network was " + network)
+            override fun onLost(network: Network) {
+                Log.d(
+                    TAG,
+                    "The application no longer has a default network. The last default network was " + network
+                )
+                isNetworkConnected = false
             }
 
-            override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
+            override fun onCapabilitiesChanged(
+                network: Network,
+                networkCapabilities: NetworkCapabilities
+            ) {
                 Log.d(TAG, "The default network changed capabilities: " + networkCapabilities)
             }
 
-            override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
+            override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
                 Log.d(TAG, "The default network changed link properties: " + linkProperties)
             }
         })
